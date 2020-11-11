d1025cbfbd3877fa50c3fb0e768c9cef7f6b9630
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-11-11 17:57:14 -0500

    Actually fix purgecss this time

--- postcss.config.js
+++ postcss.config.js
@@ -14,6 +14,6 @@ const production = process.env.NODE_ENV !== 'development';
 module.exports = {
   plugins: [
     require('tailwindcss'),
-    ...(production ? [purgecss] : [])
+    ...[purgecss]
   ]
 };
