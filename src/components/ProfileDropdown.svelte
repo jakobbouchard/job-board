bb77cf910712eb212d3d346ae9bc515828ff3ea6
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-11-11 14:42:39 -0500

    Put styles in classes for better readability

--- src/components/ProfileDropdown.svelte
+++ src/components/ProfileDropdown.svelte
@@ -34,37 +34,59 @@
   });
 </script>
 
-<div class="relative" bind:this={menu}>
+<style>
+  @layer components {
+    .profile-icon {
+      @apply flex text-sm border-2 border-transparent rounded-full
+        transition duration-150 ease-in-out;
+    }
+    .profile-icon:focus {
+      @apply outline-none border-white;
+    }
+
+    .profile-icon > img {
+      @apply h-10 w-10 rounded-full;
+    }
+
+    .dropdown {
+      @apply z-50 origin-top-right absolute right-0 w-48 py-2 mt-1 bg-white
+        rounded shadow-md;
+    }
+
+    .dropdown-item {
+      @apply block px-4 py-2 text-sm leading-5 text-gray-700
+        transition duration-150 ease-in-out;
+    }
+    .dropdown-item:hover {
+      @apply bg-gray-100;
+    }
+    .dropdown-item:focus {
+      @apply outline-none bg-gray-100 ;
+    }
+  }
+</style>
+
+<div class="relative" bind:this={ menu }>
   <div>
     <button
       on:click={() => (show = !show)}
-      class="flex text-sm border-2 border-transparent rounded-full
-        focus:outline-none focus:border-white transition duration-150
-        ease-in-out"
+      class="profile-icon"
     >
-      <img class="h-10 w-10 rounded-full" src={photoURL} alt={displayName}/>
+      <img referrerpolicy="no-referrer" src={ photoURL } alt={ displayName }/>
     </button>
 
     {#if show}
       <div
         in:scale={{ duration: 100, start: 0.95 }}
         out:scale={{ duration: 75, start: 0.95 }}
-        class="z-50 origin-top-right absolute right-0 w-48 py-2 mt-1 bg-white
-          rounded shadow-md"
+        class="dropdown"
       >
-        <a
-          href="/profile"
-          class="block px-4 py-2 text-sm leading-5 text-gray-700
-            hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition
-            duration-150 ease-in-out"
-        >Profile</a>
-        <a
-          href="."
-          class="block px-4 py-2 text-sm leading-5 text-gray-700
-            hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition
-            duration-150 ease-in-out"
-          on:click={ () => auth.signOut() }
-        >Sign out</a>
+        <a href="/profile" class="dropdown-item">
+          Profile
+        </a>
+        <a href="." class="dropdown-item" on:click={ () => auth.signOut() }>
+          Sign out
+        </a>
       </div>
     {/if}
   </div>
