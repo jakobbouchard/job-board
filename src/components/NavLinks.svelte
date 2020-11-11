ad56afbff506e2fde4fb575779406d0b511a906f
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-11-11 17:27:06 -0500

    Add contracts page

--- src/components/NavLinks.svelte
+++ src/components/NavLinks.svelte
@@ -31,4 +31,5 @@
 
 <a href="." class="nav-link" aria-current="{segment === undefined ? 'page' : undefined}">Home</a>
 <a href="/jobs" class="nav-link" aria-current="{segment === 'jobs' ? 'page' : undefined}">Jobs</a>
+<a href="/contracts" class="nav-link" aria-current="{segment === 'contracts' ? 'page' : undefined}">Contracts</a>
 <a href="/about" class="nav-link" aria-current="{segment === 'about' ? 'page' : undefined}">About</a>
